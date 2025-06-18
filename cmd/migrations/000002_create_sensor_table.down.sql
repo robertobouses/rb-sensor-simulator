d25a588c484bd95718bafaf3415ec34d8371ff45
robertobouses <robertobouses@gmail.com>
2025-06-18 13:31:53 +0200

    Add migrations, PostgreSQL connection setup, and Docker Compose configuration for database management

---
+++ cmd/migrations/000002_create_sensor_table.down.sql
@@ -0,0 +1,3 @@
+BEGIN;
+DROP TABLE IF EXISTS sen.sensor;
+COMMIT;
---
+++ cmd/migrations/000002_create_sensor_table.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+CREATE TABLE IF NOT EXISTS sen.sensor (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    sampling_interval INTEGER NOT NULL,
+    alert_threshold REAL NOT NULL,
+    unit TEXT NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    last_reading_timestamp TIMESTAMPTZ,
+    last_reading_value REAL,
+    last_reading_error TEXT
+);
+COMMIT;
